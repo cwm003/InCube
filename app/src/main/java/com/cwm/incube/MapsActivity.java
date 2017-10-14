c37b815f070efe1716c438ec55bc8543cf557376
CWM <warakornchamp@gmail.com>
2017-10-14 21:54:52 +0700

    Added add and remove marker, draw some polyline

--- app/src/main/java/com/cwm/incube/MapsActivity.java
+++ app/src/main/java/com/cwm/incube/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.cwm.incube;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.Manifest;
 import android.content.Context;
@@ -15,12 +16,25 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.GoogleMap.OnPolygonClickListener;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.cwm.incube.R.id.map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    List<LatLng> listLatLng = new ArrayList<LatLng>();
+    PolylineOptions polylineOptions = new PolylineOptions();
+    Polyline polyLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //polylineOptions.color(Color.RED);
+        googleMap.getUiSettings().setMapToolbarEnabled(false);
+
+        showCurrentLocation();
+        addMarkerOnMapLongClick();
+        deleteMarkerOnClick();
+
+        PolygonOptions rectOptions = new PolygonOptions()
+                .add(new LatLng(18.796627, 98.952543),
+                        new LatLng(18.794991, 98.952528),
+                        new LatLng(18.794984, 98.953248),
+                        new LatLng(18.796456, 98.953225))
+                .strokeWidth(3)
+                .strokeColor(Color.argb(200, 0, 255, 0))
+                .fillColor(Color.argb(128, 0, 255, 0));
+
+        Polygon polygon = mMap.addPolygon(rectOptions);
+        polygon.setClickable(true);
+
+        mMap.setOnPolygonClickListener(new OnPolygonClickListener() {
+            public void onPolygonClick(Polygon polygon) {
+                polygon.setFillColor(Color.argb(50, 255, 0, 0));
+            }
+        });
+
     }
 
     private void checkPermission() {
@@ -40,11 +85,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
+    private void showCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
             LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -56,4 +97,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    private void addMarkerOnMapLongClick() {
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            public void onMapLongClick(LatLng latLng) {
+                addMarker(latLng);
+            }
+        });
+    }
+
+    private void addMarker(LatLng latLng){
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latLng.latitude, latLng.longitude))
+                .draggable(true));
+        listLatLng.add(latLng);
+        polylineOptions.add(latLng);
+        if(polyLine != null){
+            polyLine.remove();
+        }polyLine = mMap.addPolyline(polylineOptions);
+    }
+
+    private void deleteMarkerOnClick(){
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener(){
+            public boolean onMarkerClick(Marker marker) {
+                marker.remove();
+                listLatLng.remove(marker.getPosition());
+                return true;
+            }
+        });
+    }
 }
